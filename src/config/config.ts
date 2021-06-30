dfb8c3186689c1b00d3f0fc64460285a20e2e342
moshud-lanre <lanremoshud@gmail.com>
2021-06-30 18:54:16 +0100

    Environment variable refactor

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,12 +4,12 @@ export const config = {
   "dev": {
     "username": process.env.POSTGRES_USERNAME,
     "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.Database,
-    "host": process.env.Host,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.Bucket
+    "aws_media_bucket": process.env.BUCKET
   },
   "prod": {
     "username": "",
